89d0bebc165ebcbbb155f48fa1c9c727ac9d77ad
MelidaZ <69802296+MelidaZ@users.noreply.github.com>
2021-07-15 13:37:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ require('dotenv').config();
     client.on('ready', async () => {
         await client.setActivity({
             details: "In Game", 
+             buttons: [{
+                    label: #Text,
+                    url: #url
+                },
+                {
+                    label: #secondtext,
+                    url: #secoundurl
+                }
+            ],
+            timestamps: {
+                start: rpc.timestamps.useTimer ? Number(rpc.timestamps.startTimestamp) || Number(startTimestamp) : undefined,
+                end: rpc.timestamps.useTimer ? Number(rpc.timestamps.endTimestamp) : undefined
+            },
+            instance: true
+        }
         }).catch(err => console.log(err))
         console.log("Ready!");
     });
